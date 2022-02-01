ab45994a2e8b1803fd06f7f898de03f30f0447d7
syahshiimi <syahrulanuar@Syahruls-MacBook-Air.local>
2022-02-02 01:08:04 +0800

    started on archives map

--- src/pages/browsemap.js
+++ src/pages/browsemap.js
@@ -1,26 +1,47 @@
 import React from "react";
 import Layout from "../components/Layout";
 import styled from "styled-components";
+import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+import asia from "../../content/asia.json";
 
 /////////////////////////////////
 /////// Tablet & Desktop Only ///
 /////////////////////////////////
-function BrowseMap() {
+const BrowseMap = () => {
   return (
     <Layout>
       <BrowseMapWrapper>
         <h1 className="c-browsemap__title">Browse Archive Map</h1>
+        <ComposableMap
+          className="c-browsemap"
+          projectionConfig={{ scale: 200 }}
+        >
+          <Geographies geography={asia}>
+            {({ geographies }) =>
+              geographies.map((geo) => (
+                <Geography key={geo.rsmKey} geography={geo} />
+              ))
+            }
+          </Geographies>
+        </ComposableMap>
       </BrowseMapWrapper>
     </Layout>
   );
-}
+};
 
 const BrowseMapWrapper = styled.article`
   display: none;
   @media (min-width: 902px) {
+    padding: 4vh var(--padding-mobile) 6vh var(--padding-mobile);
     display: grid;
     place-self: center-stretch;
     text-align: center;
+
+    .c-browsemap {
+      padding: 1vh 5vw;
+      border: 1px solid var(--primary-clr-200);
+      border-radius: 25px;
+    }
   }
 `;
 
