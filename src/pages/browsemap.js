449a59c77b2f53b98e41c4670fe6ec4f328ccca9
syahshiimi <syahrulanuar@Syahruls-MacBook-Air.local>
2022-02-08 18:40:44 +0800

    cleaned up src folder and started working on browsemap.js page

--- src/pages/browsemap.js
+++ src/pages/browsemap.js
@@ -1,28 +1,52 @@
 import React from "react";
 import Layout from "../components/Layout";
 import styled from "styled-components";
-import { ComposableMap, Geographies, Geography } from "react-simple-maps";
-import asia from "../../content/asia.json";
+import {
+  ComposableMap,
+  ZoomableGroup,
+  Geographies,
+  Geography,
+  Marker,
+} from "react-simple-maps";
+import ReactTooltip from "react-tooltip";
+import asia from "../../content/asia-mapshaper.json";
 
 /////////////////////////////////
 /////// Tablet & Desktop Only ///
 /////////////////////////////////
 const BrowseMap = () => {
+  const markers = [
+    {
+      markerOffset: -15,
+      name: "Japan",
+      coordinates: [138.2529, 36.2048],
+    },
+  ];
   return (
     <Layout>
       <BrowseMapWrapper>
         <h1 className="c-browsemap__title">Browse Archive Map</h1>
-        <ComposableMap
-          className="c-browsemap"
-          projectionConfig={{ scale: 200 }}
-        >
-          <Geographies geography={asia}>
-            {({ geographies }) =>
-              geographies.map((geo) => (
-                <Geography key={geo.rsmKey} geography={geo} />
-              ))
-            }
-          </Geographies>
+        <ComposableMap className="c-browsemap" data-tip="" height={400}>
+          <ZoomableGroup center={[80, 20]} zoom={1.5}>
+            {" "}
+            <Geographies geography={asia}>
+              {({ geographies }) =>
+                geographies.map((geo) => (
+                  <Geography key={geo.rsmKey} geography={geo} />
+                ))
+              }
+            </Geographies>
+            {markers.map(({ name, coordinates, markerOffset }) => {
+              console.log(name, coordinates, markerOffset);
+              return (
+                <Marker key={name} coordinates={coordinates}>
+                  <text textAnchor="middle" y={markerOffset} fill="#F53">
+                    {name}
+                  </text>
+                </Marker>
+              );
+            })}
+          </ZoomableGroup>
         </ComposableMap>
       </BrowseMapWrapper>
     </Layout>
@@ -34,14 +58,23 @@ const BrowseMapWrapper = styled.article`
   @media (min-width: 902px) {
     padding: 4vh var(--padding-mobile) 6vh var(--padding-mobile);
     display: grid;
-    place-self: center-stretch;
     text-align: center;
+  }
 
-    .c-browsemap {
-      padding: 1vh 5vw;
-      border: 1px solid var(--primary-clr-200);
-      border-radius: 25px;
-    }
+  .c-browsemap {
+    display: flex;
+    text-align: center;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: var(--secondary-clr-250);
+    /* background-color: var(--primary-clr-50); */
+    /* border: 1px solid black; */
+    padding: 0vh 1vw;
+  }
+  .rsm-geography {
+    display: flex;
+    justify-content: center;
   }
 `;
 
