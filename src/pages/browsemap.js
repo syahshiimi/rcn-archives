8888e67b93706795ccba8fdb4eeb4230fb5d7ba0
syahshiimi <syahrulanuar@Syahruls-MacBook-Air.local>
2022-02-09 13:49:58 +0800

    started working on browsemap annotations and markers

--- src/pages/browsemap.js
+++ src/pages/browsemap.js
@@ -7,47 +7,76 @@ import {
   Geographies,
   Geography,
   Marker,
+  Annotation,
 } from "react-simple-maps";
 import ReactTooltip from "react-tooltip";
 import asia from "../../content/asia-mapshaper.json";
+import { countryData } from "../data";
 
 /////////////////////////////////
 /////// Tablet & Desktop Only ///
 /////////////////////////////////
 const BrowseMap = () => {
-  const markers = [
-    {
-      markerOffset: -15,
-      name: "Japan",
-      coordinates: [138.2529, 36.2048],
-    },
-  ];
   return (
     <Layout>
       <BrowseMapWrapper>
         <h1 className="c-browsemap__title">Browse Archive Map</h1>
-        <ComposableMap className="c-browsemap" data-tip="" height={400}>
-          <ZoomableGroup center={[80, 20]} zoom={1.5}>
-            {" "}
-            <Geographies geography={asia}>
-              {({ geographies }) =>
-                geographies.map((geo) => (
-                  <Geography key={geo.rsmKey} geography={geo} />
-                ))
-              }
-            </Geographies>
-            {markers.map(({ name, coordinates, markerOffset }) => {
-              console.log(name, coordinates, markerOffset);
-              return (
-                <Marker key={name} coordinates={coordinates}>
-                  <text textAnchor="middle" y={markerOffset} fill="#F53">
-                    {name}
-                  </text>
-                </Marker>
-              );
-            })}
-          </ZoomableGroup>
-        </ComposableMap>
+        <div className="l-browsemap">
+          {" "}
+          <ComposableMap
+            className="c-browsemap"
+            data-tip=""
+            // height={300}
+            // width={900}
+          >
+            <ZoomableGroup center={[85, 20]} zoom={2}>
+              {" "}
+              <Geographies geography={asia}>
+                {({ geographies }) =>
+                  geographies.map((geo) => (
+                    <Geography key={geo.rsmKey} geography={geo} />
+                  ))
+                }
+              </Geographies>
+              {countryData.map(({ name, coordinates, markerOffset }) => {
+                console.log(name, coordinates, markerOffset);
+                return (
+                  <Marker key={name} coordinates={coordinates}>
+                    <circle className="c-browsemap__marker"></circle>
+                  </Marker>
+                );
+              })}
+              {countryData.map(
+                ({ name, coordinates, dx, dy, curve, textY }) => {
+                  console.log(name, coordinates, curve);
+                  return (
+                    <Annotation
+                      key={name}
+                      subject={coordinates}
+                      dx={dx}
+                      dy={dy}
+                      curve={curve}
+                      connectorProps={{
+                        stroke: "#242423",
+                        strokeWidth: 1,
+                        strokeLinecap: "round",
+                      }}
+                    >
+                      <text
+                        className="c-browsemap__annotateText"
+                        x={textY}
+                        textAnchor="middle"
+                        alignmentBaseline="middle"
+                      >
+                        {name}
+                      </text>
+                    </Annotation>
+                  );
+                }
+              )}
+            </ZoomableGroup>
+          </ComposableMap>
+        </div>
       </BrowseMapWrapper>
     </Layout>
   );
@@ -59,22 +88,46 @@ const BrowseMapWrapper = styled.article`
     padding: 4vh var(--padding-mobile) 6vh var(--padding-mobile);
     display: grid;
     text-align: center;
-  }
 
-  .c-browsemap {
-    display: flex;
-    text-align: center;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    background-color: var(--secondary-clr-250);
-    /* background-color: var(--primary-clr-50); */
-    /* border: 1px solid black; */
-    padding: 0vh 1vw;
-  }
-  .rsm-geography {
-    display: flex;
-    justify-content: center;
+    .l-browsemap {
+      border: 1px solid black;
+      border-radius: calc(8vh);
+    }
+    .c-browsemap__title {
+      margin-bottom: 6vh;
+    }
+
+    .c-browsemap {
+      display: flex;
+      text-align: center;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      /* background-color: var(--secondary-clr-250); */
+      /* background-color: var(--primary-clr-50); */
+      padding: 0vh 1vw;
+      height: 100;
+    }
+    .rsm-geographies {
+      fill: var(--primary-clr-100);
+      stroke: var(--primary-clr-150);
+      display: flex;
+      justify-content: center;
+    }
+
+    .c-browsemap__marker {
+      fill: var(--primary-clr-50);
+      stroke: var(--primary-clr-150);
+      r: 8;
+    }
+
+    .c-browsemap__marker:hover {
+      fill: var(--primary-clr-150);
+    }
+    .c-browsemap__annotateText {
+      font-size: 0.6rem;
+      font-family: "Ubuntu", sans-serif;
+    }
   }
 `;
 
