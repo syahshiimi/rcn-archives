5b63c2c3e746253c55d9c54d2c49672e399e4817
syahshiimi <syahrulanuar@Syahruls-MacBook-Air.local>
2022-02-24 08:37:48 +0000

    added cloud plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -128,6 +128,18 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-gatsby-cloud`,
+      options: {
+        headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
+        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+        mergeSecurityHeaders: true, // boolean to turn off the default security headers
+        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
+        mergeCachingHeaders: true, // boolean to turn off the default caching headers
+        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
+      },
+    },
+
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         icon: "src/assets/apple-touch-icon.png",
